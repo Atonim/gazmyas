d179d74f8176d6ba236727ba0c157de8ad352bd6
Atonim <Atonimaurus@gmail.com>
2025-03-22 19:27:14 +0300

    немного изменил

--- ai/agent.js
+++ ai/agent.js
@@ -1,202 +1,231 @@
-const Msg = require('./msg')
-const readline = require('readline')
-const EnvirenmentAnalizer = require('./envirenmentAnalyzer/envirenmentAnalyzer')
+const Msg = require("./msg");
+const readline = require("readline");
+const EnvirenmentAnalizer = require("./envirenmentAnalyzer/envirenmentAnalyzer");
 
-const SIGNS_AFTER_COMA = 2
-const round = (number) => Math.round(number * 10 * SIGNS_AFTER_COMA) / (10 * SIGNS_AFTER_COMA)
+const SIGNS_AFTER_COMA = 2;
+const round = (number) =>
+  Math.round(number * 10 * SIGNS_AFTER_COMA) / (10 * SIGNS_AFTER_COMA);
 
 const actions = [
-	{ act: 'flag', fl: 'frb' },
-	{ act: 'flag', fl: 'gl' },
-	{ act: 'flag', fl: 'fc' },
-	{ act: 'kick', fl: 'b', goal: 'gr' },
-]
+  { act: "flag", fl: "frb" },
+  { act: "flag", fl: "gl" },
+  { act: "flag", fl: "fc" },
+  { act: "kick", fl: "b", goal: "gr" },
+];
 
 class Agent {
-	constructor(teamName, number, rotatespeed = 20) {
-		this.position = 'l' // По умолчанию ~ левая половина поля
-		this.run = false // Игра начата
-		this.act = null // Действия
-		this.rl = readline.createInterface({
-			// Чтение консоли
-			input: process.stdin,
-			output: process.stdout,
-		})
-		this.rl.on('line', (input) => {
-			// Обработка строки из кон—
-			if (this.run) {
-				// Если игра начата
-				// Движения вперед, вправо, влево, удар по мячу
+  constructor(teamName, number, rotatespeed = 20) {
+    this.position = "l"; // По умолчанию ~ левая половина поля
+    this.run = false; // Игра начата
+    this.act = null; // Действия
+    this.rl = readline.createInterface({
+      // Чтение консоли
+      input: process.stdin,
+      output: process.stdout,
+    });
+    this.rl.on("line", (input) => {
+      // Обработка строки из кон—
+      if (this.run) {
+        // Если игра начата
+        // Движения вперед, вправо, влево, удар по мячу
 
-				if ('w' == input) this.act = { n: 'dash', v: 100 }
-				if ('d' == input) this.act = { n: 'turn', v: 20 }
-				if ('a' == input) this.act = { n: 'turn', v: -20 }
-				if ('s' == input) this.act = { n: 'kick', v: 100 }
-			}
-		})
-		this.rotatespeed = rotatespeed
-		this.envirenmentAnalizer = new EnvirenmentAnalizer(teamName, this.position)
-		this.team = teamName
-		this.number = number
-		this.strategy = 'flag'
-		this.strategyIdx = 0
-	}
+        if ("w" == input) this.act = { n: "dash", v: 100 };
+        if ("d" == input) this.act = { n: "turn", v: 20 };
+        if ("a" == input) this.act = { n: "turn", v: -20 };
+        if ("s" == input) this.act = { n: "kick", v: 100 };
+      }
+    });
+    this.rotatespeed = rotatespeed;
+    this.envirenmentAnalizer = new EnvirenmentAnalizer(teamName, this.position);
+    this.team = teamName;
+    this.number = number;
+    this.strategy = "flag";
+    this.strategyIdx = 0;
+  }
   msgGot(msg) {
-		// Получение сообщения
-		let data = msg.toString('utf8') // ПРиведение
-		this.processMsg(data) // Разбор сообщения
-		this.sendCmd() // Отправка команды
-	}
-	setSocket(socket) {
-		// Настройка сокета
-		this.socket = socket
-	}
-	socketSend(cmd, value) {
-		// Отправка команды
-		this.socket.sendMsg(`(${cmd} ${value})`)
-	}
-	processMsg(msg) {
-		// Обработка сообщения
-		let data = Msg.parseMsg(msg) // Разбор сообщения
-		if (!data) throw new Error('Parse error\n' + msg)
-		// Первое (hear) — начало игры
-		if (data.cmd == 'hear') this.run = true
-		if (data.cmd == 'init') this.initAgent(data.p) //MHMnmaflM3auMH
-		this.analyzeEnv(data.msg, data.cmd, data.p) // Обработка
-	}
-	initAgent(p) {
-		if (p[0] == 'r') {
-			this.position = 'r' // Правая половина поля
-			this.envirenmentAnalizer.fieldSide = 'r'
-		}
-		if (p[1]) this.id = p[1] // id игрока
-	}
-	analyzeEnv(msg, cmd, p) {
-		if (cmd == 'hear' && p[2].includes('goal')) {
-			this.run = false
-			this.strategyIdx = 0
-			this.strategy = actions[0].act
-		}
-		if (cmd === 'see') {
-			this.envirenmentAnalizer.analyzeVisibleInformation(p, this.position)
-		}
-		if (this.run) {
-			switch (this.strategy) {
-				case 'flag':
-					this.searchFlag(actions[this.strategyIdx].fl)
-					this.turnToFlag(actions[this.strategyIdx].fl)
-					this.moveToFlag(actions[this.strategyIdx].fl)
-					break
-				case 'kick':
-					this.searchBall()
-					this.moveToBall()
-					// this.go
-					break
-			}
-		}
-		console.log(this.printInfo())
-	}
+    // Получение сообщения
+    let data = msg.toString("utf8"); // ПРиведение
+    this.processMsg(data); // Разбор сообщения
+    this.sendCmd(); // Отправка команды
+  }
+  setSocket(socket) {
+    // Настройка сокета
+    this.socket = socket;
+  }
+  socketSend(cmd, value) {
+    // Отправка команды
+    console.log("send ", cmd, value);
+    this.socket.sendMsg(`(${cmd} ${value})`);
+  }
+  processMsg(msg) {
+    // Обработка сообщения
+    let data = Msg.parseMsg(msg); // Разбор сообщения
+    //console.log(data);
+    if (!data) throw new Error("Parse error\n" + msg);
+    // Первое (hear) — начало игры
+    if (data.cmd == "hear") this.run = true;
+    if (data.cmd == "init") this.initAgent(data.p); //MHMnmaflM3auMH
+    this.analyzeEnv(data.msg, data.cmd, data.p); // Обработка
+  }
+  initAgent(p) {
+    if (p[0] == "r") {
+      this.position = "r"; // Правая половина поля
+      this.envirenmentAnalizer.fieldSide = "r";
+    }
+    if (p[1]) this.id = p[1]; // id игрока
+  }
+  analyzeEnv(msg, cmd, p) {
+    if (cmd == "hear" && p[2].includes("goal")) {
+      this.run = false;
+      this.strategyIdx = 0;
+      this.strategy = actions[0].act;
+    }
+    if (cmd === "see") {
+      this.envirenmentAnalizer.analyzeVisibleInformation(p);
+    }
+    if (this.run) {
+      switch (this.strategy) {
+        case "flag":
+          this.searchFlag(actions[this.strategyIdx].fl);
+          break;
+        case "kick":
+          this.searchBall();
+          this.moveToBall();
+          // this.go
+          break;
+      }
+    }
+    console.log(this.printInfo());
+  }
 
-	searchFlag (flagName) {
-		let flagIndex = this.envirenmentAnalizer.visibleFlags.findIndex((elem) => elem[4] === flagName)
-		if (flagIndex === -1) {
-			this.act = { n: 'turn', v: 20 }
-		}
-	}
-	turnToFlag (flagName) {
-		let flagIndex = this.envirenmentAnalizer.visibleFlags.findIndex((elem) => elem[4] === flagName)
-		if (flagIndex !== -1) {
-			const aTarget = this.envirenmentAnalizer.visibleFlags[flagIndex][3]
-			this.act = { n: 'turn', v: aTarget }
-		}
-	}
-	moveToFlag (flagName) {
-		let flagIndex = this.envirenmentAnalizer.visibleFlags.findIndex((elem) => elem[4] === flagName)
-		if (flagIndex !== -1) {
-			const target = this.envirenmentAnalizer.visibleFlags[flagIndex]
-			const aTarget = target[3]
-			const dTarget = target[2]
-			if (dTarget > 70) {
-				this.act = { n: 'dash', v: 70 }
-			} else if (dTarget > 40 && Math.abs(aTarget) < 30) {
-				this.act = { n: 'dash', v: 70 }
-			} else if (dTarget > 20 && Math.abs(aTarget) < 20) {
-				this.act = { n: 'dash', v: 100 }
-			} else if (dTarget > 3 && Math.abs(aTarget) < 5) {
-				this.act = { n: 'dash', v: 100 }
-			} else if (dTarget < 3) {
-				this.goNextAction()
-			}
-		}
-	}
-	searchBall () {
-		const isBallFound = this.envirenmentAnalizer.isBallSeen
-		if (!isBallFound) {
-			this.act = { n: 'turn', v: 20 }
-		}
-	}
-	moveToBall () {
-		const isBallFound = this.envirenmentAnalizer.isBallSeen
-		if (isBallFound) {
-			const gate = actions[this.strategyIdx].goal
-			const gateIdx = this.envirenmentAnalizer.visibleFlags.findIndex((elem) => elem[4] === gate)
-			const aBall = this.envirenmentAnalizer.ballCoords.a
-			const dBall = this.envirenmentAnalizer.ballCoords.d
-			if (dBall > 70) {
-				this.act = { n: 'dash', v: 70 }
-			} else if (Math.abs(aBall) > 20) {
-				this.act = { n: 'turn', v: aBall }
-			} else if (dBall > 0.5) {
-				this.act = { n: 'dash', v: 100 }
-			} else if (gateIdx !== -1) {
-				this.act = { n: 'kick', v: `10 20` }
-			} else {
-				const aGate = this.envirenmentAnalizer.visibleFlags[gateIdx][3]
-				const dGate = this.envirenmentAnalizer.visibleFlags[gateIdx][2]
-				if (dGate > 25) {
-					this.act = { n: 'kick', v: `10 ${aGate}` }
-				} else {
-					this.sct = { n: 'kick', v: `100 ${aGate}` }
-				}
-			}
-		}
-	}
-	goNextAction () {
-		this.strategyIdx = (this.strategyIdx + 1) % actions.length
-		this.strategy = actions[this.strategyIdx].act
-		console.log("compleated")
-		console.log(this.strategy, this.strategyIdx)
-	}
+  searchFlag(flagName) {
+    //console.log(this.envirenmentAnalizer.visibleFlags);
+    let flagIndex = this.envirenmentAnalizer.visibleFlags.findIndex(
+      (elem) => elem[4] === flagName
+    );
+    if (flagIndex === -1) {
+      this.keepSearchig();
+    } else {
+      //this.turnToFlag(flagIndex);
+      this.moveToFlag(flagIndex);
+    }
+  }
+  keepSearchig() {
+    this.act = { n: "turn", v: 20 };
+    //sendCmd();
+  }
+  turnToFlag(flagIndex) {
+    const angle = this.envirenmentAnalizer.visibleFlags[flagIndex][3];
+    this.act = { n: "turn", v: angle };
+  }
+  moveToFlag(flagIndex) {
+    const target = this.envirenmentAnalizer.visibleFlags[flagIndex];
+    const angle = target[3];
+    const distance = target[2];
+    //if (distance > 70) {
+    //  this.act = { n: "dash", v: 70 };
+    //} else if (distance > 40 && Math.abs(angle) < 30) {
+    //  this.act = { n: "dash", v: 70 };
+    //} else if (distance > 20 && Math.abs(angle) < 20) {
+    //  this.act = { n: "dash", v: 100 };
+    //} else if (distance > 3 && Math.abs(angle) < 5) {
+    //  this.act = { n: "dash", v: 100 };
+    //} else if (distance < 3) {
+    //  this.goNextAction();
+    //}
+    console.log("distance ", distance, " angle ", angle);
+    if (Math.abs(angle) > 20) {
+      this.act = { n: "turn", v: angle };
+    } else if (Math.abs(angle) < 5) {
+      this.act = { n: "dash", v: 100 };
+    } else if (Math.abs(angle) <= 10 && distance > 70) {
+      this.act = { n: "dash", v: 100 };
+    } else if (Math.abs(angle) <= 10 && distance > 50) {
+      this.act = { n: "dash", v: 80 };
+    } else if (Math.abs(angle) <= 10 && distance > 30) {
+      this.act = { n: "dash", v: 60 };
+    } else if (Math.abs(angle) <= 20 && distance > 50) {
+      this.act = { n: "dash", v: 80 };
+    } else if (distance < 3) {
+      this.goNextAction();
+    } else {
+      this.act = { n: "turn", v: angle };
+    }
+  }
+  searchBall() {
+    const isBallFound = this.envirenmentAnalizer.isBallSeen;
+    if (!isBallFound) {
+      this.act = { n: "turn", v: 20 };
+    }
+  }
+  moveToBall() {
+    const isBallFound = this.envirenmentAnalizer.isBallSeen;
+    if (isBallFound) {
+      const gate = actions[this.strategyIdx].goal;
+      const gateIdx = this.envirenmentAnalizer.visibleFlags.findIndex(
+        (elem) => elem[4] === gate
+      );
+      const aBall = this.envirenmentAnalizer.ballCoords.a;
+      const dBall = this.envirenmentAnalizer.ballCoords.d;
+      if (dBall > 70) {
+        this.act = { n: "dash", v: 70 };
+      } else if (Math.abs(aBall) > 20) {
+        this.act = { n: "turn", v: aBall };
+      } else if (dBall > 0.5) {
+        this.act = { n: "dash", v: 100 };
+      } else if (gateIdx !== -1) {
+        this.act = { n: "kick", v: `10 20` };
+      } else {
+        const aGate = this.envirenmentAnalizer.visibleFlags[gateIdx][3];
+        const dGate = this.envirenmentAnalizer.visibleFlags[gateIdx][2];
+        if (dGate > 25) {
+          this.act = { n: "kick", v: `10 ${aGate}` };
+        } else {
+          this.sct = { n: "kick", v: `100 ${aGate}` };
+        }
+      }
+    }
+  }
+  goNextAction() {
+    console.log("strategy completed: ", this.strategy, " ", this.strategyIdx);
+    this.strategyIdx = (this.strategyIdx + 1) % actions.length;
+    this.strategy = actions[this.strategyIdx].act;
+    console.log("next strategy: ", this.strategy, " ", this.strategyIdx);
+  }
 
+  sendCmd() {
+    if (this.run) {
+      // Игра начата
+      if (this.act) {
+        if (this.act.n == "kick")
+          this.socketSend(this.act.n, this.act.v + " 0");
+        else this.socketSend(this.act.n, this.act.v);
+      }
 
+      this.act = null; // Сброс команды
+    }
+  }
 
+  printInfo() {
+    let report = `(Team ${this.team} player №${
+      this.number
+    }): My position (x: ${round(this.envirenmentAnalizer.x)}, y: ${round(
+      this.envirenmentAnalizer.y
+    )}), side: ${this.envirenmentAnalizer.fieldSide}. I see:`;
 
-	sendCmd() {
-		if (this.run) {
-			// Игра начата
-			if (this.act) {
-				if (this.act.n == 'kick')
-					this.socketSend(this.act.n, this.act.v + " 0")
-				else
-					this.socketSend(this.act.n, this.act.v)
-			}
-      
-			this.act = null // Сброс команды
-		}
-	}
-
-	printInfo() {
-		let report = `(Team ${this.team} player №${this.number}): My position (x: ${round(this.envirenmentAnalizer.x)}, y: ${round(this.envirenmentAnalizer.y)}), side: ${this.envirenmentAnalizer.fieldSide}. I see:`
-		
-		if (!!this.envirenmentAnalizer.opponents.length) {
-			for (let playerPos of this.envirenmentAnalizer.opponents) {
-				report += `\n(Team ${this.team} player №${this.number}):\t\tOpponent - position (x: ${round(playerPos.x)}, y: ${round(playerPos.y)})`
-			}
-		} else {
-			report += ` Nothing`
-		}
-		return report
-	}
+    if (!!this.envirenmentAnalizer.opponents.length) {
+      for (let playerPos of this.envirenmentAnalizer.opponents) {
+        report += `\n(Team ${this.team} player №${
+          this.number
+        }):\t\tOpponent - position (x: ${round(playerPos.x)}, y: ${round(
+          playerPos.y
+        )})`;
+      }
+    } else {
+      report += ` Nothing`;
+    }
+    return report;
+  }
 }
 
-module.exports = Agent // Экспорт игрока
+module.exports = Agent; // Экспорт игрока
--- ai/envirenmentAnalyzer/envirenmentAnalyzer.js
+++ ai/envirenmentAnalyzer/envirenmentAnalyzer.js
@@ -1,108 +1,110 @@
-const Flags = require('./flags')
-const CalcPos = require('./positionCalculator')
+const Flags = require("./flags");
+const CalcPos = require("./positionCalculator");
 
 class EnvirenmentAnalyzer {
-    constructor(team, side = 'l') {
+  constructor(team, side = "l") {
+    this.time = 0;
+    this.team = team;
+    this.fieldSide = side;
+    this.x = Infinity;
+    this.y = Infinity;
 
-        this.time = 0
-        this.team = team
-        this.fieldSide = side
-        this.x = Infinity
-        this.y = Infinity
+    this.visibleFlags = [];
+    this.teammates = [];
+    this.opponents = [];
 
-        this.visibleFlags = []
-        this.teammates = []
-        this.opponents = []
+    this.isBallSeen = false;
+    this.ballCoords = { x: NaN, y: NaN, d: NaN, a: NaN };
+  }
+  getPlayerCoords() {
+    let [x, y] = [this.x, this.y];
+    if (this.fieldSide === "r") {
+      return [-x, -y];
+    }
+    return [x, y];
+  }
+  changeCoordsBySide(coords) {
+    if (this.fieldSide === "r") {
+      return [-coords[0], -coords[1]];
+    }
+    return [coords[0], coords[1]];
+  }
 
-        this.isBallSeen = false
-        this.ballCoords = {x: NaN, y: NaN, d: NaN, a: NaN}
-    }
-    getPlayerCoords () {
-        let [x, y] = [this.x, this.y]
-        if (this.fieldSide === 'r') {
-            return [-x, -y]
-        }
-        return [x, y]
-    }
-    changeCoordsBySide (coords) {
-        if (this.fieldSide === 'r') {
-            return [-coords[0], -coords[1]]
-        }
-        return [coords[0], coords[1]]
-    }
+  analyzeVisibleInformation(information) {
+    this.opponents = [];
+    this.visibleFlags = [];
+    information = this.__parseTimeFrom(information);
+    this.__parseVisible(information);
 
-    analyzeVisibleInformation (information) {
-        this.opponents = []
-        this.visibleFlags = []
-        information = this.__parseTimeFrom(information)
-        this.__parseVisible(information)
-        
-        this.__calculateAgentPosition()
-    }
+    this.__calculateAgentPosition();
+  }
 
-    __parseTimeFrom (information) {
-        if (information.length > 0) {
-            this.time = information[0]
-            information.splice(0, 1)
-        }
-        return information
+  __parseTimeFrom(information) {
+    if (information.length > 0) {
+      this.time = information[0];
+      console.log(this.time, " )");
+      information.splice(0, 1);
     }
-    __parseVisible (information) {
-        for (const visibleObject of information) {
-            let visibleObjectName = visibleObject.cmd.p.join('')
+    return information;
+  }
+  __parseVisible(information) {
+    for (const visibleObject of information) {
+      let visibleObjectName = visibleObject.cmd.p.join("");
 
-            this.__parseFlagFrom(visibleObject, visibleObjectName)
-            this.__parsePlayerFrom(visibleObject, visibleObjectName)
-            this.__parseBallInfo(visibleObject, visibleObjectName)
-        }
+      this.__parseFlagFrom(visibleObject, visibleObjectName);
+      this.__parsePlayerFrom(visibleObject, visibleObjectName);
+      this.__parseBallInfo(visibleObject, visibleObjectName);
     }
-    __parseFlagFrom (visibleObject, visibleObjectName) {
-        if (Flags[visibleObjectName]) {
-            const fX = Flags[visibleObjectName].x
-            const fY = Flags[visibleObjectName].y
-            const flag = [fX, fY].concat(visibleObject.p.slice(0, 2)).concat([visibleObjectName])
-            this.visibleFlags.push(flag)
-        }
+  }
+  __parseFlagFrom(visibleObject, visibleObjectName) {
+    if (Flags[visibleObjectName]) {
+      const fX = Flags[visibleObjectName].x;
+      const fY = Flags[visibleObjectName].y;
+      const flag = [fX, fY]
+        .concat(visibleObject.p.slice(0, 2))
+        .concat([visibleObjectName]);
+      this.visibleFlags.push(flag);
     }
-    __parsePlayerFrom (visibleObject, visibleObjectName) {
-        if (visibleObjectName.startsWith('p')) {
-            const playerTeam = visibleObject.cmd.p[1]
-            const playerNumero = visibleObject.cmd.p[2]
-            const playerLocation = visibleObject.p
-            const playerPosition = this.changeCoordsBySide(
-                CalcPos.calculateObjectPosition(
-                    this.visibleFlags, 
-                    this.getPlayerCoords(), 
-                    playerLocation
-                )
-            )
-            if (playerTeam !== this.team) {
-                this.opponents.push({x: playerPosition[0], y: playerPosition[1]})
-            }
-        }
-    }
-    __parseBallInfo (visibleObject, visibleObjectName) {
-        if (visibleObjectName.startsWith('b')) {
-            this.ballCoords.d = visibleObject.p[0]
-            this.ballCoords.a = visibleObject.p[1]
-            this.isBallSeen = true
-            const [x, y] = this.changeCoordsBySide(
-                CalcPos.calculateObjectPosition(
-                    this.visibleFlags,
-                    this.getPlayerCoords(),
-                    visibleObject.p
-                )
-            )
-            this.ballCoords.x = x
-            this.ballCoords.y = y
-        }
-    }
-    
-    __calculateAgentPosition () {
-        [this.x, this.y] = this.changeCoordsBySide(
-            CalcPos.calculatePlayerPosition(this.visibleFlags)
+  }
+  __parsePlayerFrom(visibleObject, visibleObjectName) {
+    if (visibleObjectName.startsWith("p")) {
+      const playerTeam = visibleObject.cmd.p[1];
+      const playerNumero = visibleObject.cmd.p[2];
+      const playerLocation = visibleObject.p;
+      const playerPosition = this.changeCoordsBySide(
+        CalcPos.calculateObjectPosition(
+          this.visibleFlags,
+          this.getPlayerCoords(),
+          playerLocation
         )
+      );
+      if (playerTeam !== this.team) {
+        this.opponents.push({ x: playerPosition[0], y: playerPosition[1] });
+      }
     }
+  }
+  __parseBallInfo(visibleObject, visibleObjectName) {
+    if (visibleObjectName.startsWith("b")) {
+      this.ballCoords.d = visibleObject.p[0];
+      this.ballCoords.a = visibleObject.p[1];
+      this.isBallSeen = true;
+      const [x, y] = this.changeCoordsBySide(
+        CalcPos.calculateObjectPosition(
+          this.visibleFlags,
+          this.getPlayerCoords(),
+          visibleObject.p
+        )
+      );
+      this.ballCoords.x = x;
+      this.ballCoords.y = y;
+    }
+  }
+
+  __calculateAgentPosition() {
+    [this.x, this.y] = this.changeCoordsBySide(
+      CalcPos.calculatePlayerPosition(this.visibleFlags)
+    );
+  }
 }
 
-module.exports = EnvirenmentAnalyzer // Экспорт
+module.exports = EnvirenmentAnalyzer; // Экспорт
